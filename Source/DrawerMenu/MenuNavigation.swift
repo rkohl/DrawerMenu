3fb5626991cc2d29bf3f5779fcfa592650f89520
Ryan Kohl <kohldevelopmentgroup@icloud.com>
2020-05-28 03:20:16 -0500

    color

--- Source/DrawerMenu/MenuNavigation.swift
+++ Source/DrawerMenu/MenuNavigation.swift
@@ -30,7 +30,7 @@ public class MenuNavigation: UINavigationController {
     navigationBar.prefersLargeTitles = attributes.useLargeTitle
     navigationBar.layoutMargins.left = attributes.leftMargin
     navigationBar.layoutMargins.right = attributes.rightMargin
-    navigationItem.largeTitleDisplayMode  = .never
+
     navigationItem.largeTitleDisplayMode  = attributes.largeTitleDisplay
     navigationBar.shadowImage = UIImage()
     navigationBar.barTintColor = attributes.barColor
@@ -49,7 +49,7 @@ public struct NavigationBarAttributes {
     public var leftMargin: CGFloat = 16
     public var rightMargin: CGFloat = 16
     public var barColor: UIColor = .systemGroupedBackground
-    public var barBackgroundColor: UIColor = .clear
+    public var barBackgroundColor: UIColor = .systemGroupedBackground
     public var tintColor: UIColor = UIColor.init(red: 224, green: 0, blue: 56, alpha: 1)
     public var barTanslucent: Bool = false
     public var viewBackgroundColor: UIColor = .systemGroupedBackground
